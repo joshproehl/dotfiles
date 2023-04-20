f49a0d320706577eebf2b823bef93867e1ab755a
Josh Proehl <josh@daedalusdreams.com>
2023-04-20 14:34:21 -0500

    Updating heirline, adding LSP

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -18,6 +18,27 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+require('nvim-web-devicons').setup {
+  -- your personnal icons can go here (to override)
+  -- DevIcon will be appended to `name`
+  override = {
+    zsh = {
+      icon = "",
+      color = "#428850",
+      name = "Zsh",
+    },
+  };
+  -- globally enable default icons (default to false)
+  -- will get overriden by `get_icons` option
+  default = true;
+}
+
+-- Change diagnostic signs.
+vim.fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
 -- nvim-tree configuration --
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
@@ -37,6 +58,30 @@ require("nvim-tree").setup({
   },
 })
 -- END VimTree config --
+
+-- LSP configuration
+local lsp_status = require('lsp-status')
+lsp_status.register_progress()
+
+local navic = require('nvim-navic')
+
+local lspconfig = require('lspconfig')
+lspconfig.elixirls.setup{
+    -- Unix
+    cmd = { "/home/joshproehl/.lsp/elixir-ls/language_server.sh" };
+    -- Windows
+    --cmd = { "/path/to/elixir-ls/language_server.bat" };
+    on_attach = function(client, bufnr)
+      require('lsp-status').on_attach(client)
+      navic.attach(client,bufnr)
+    end
+}
+-- END LSP configuration
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+  end
+}
 
 -- heirline configuration --
 require('heirline_config')
--- home/private_dot_config/nvim/lua/heirline_config.lua
+++ home/private_dot_config/nvim/lua/heirline_config.lua
@@ -101,6 +101,68 @@ FileFlags,
 { provider = '%<'} -- this means that the statusline is cut here when there's not enough space
 )
 
+local FileEncoding = {
+  provider = function()
+    local enc = (vim.bo.fenc ~= '' and vim.bo.fenc) or vim.o.enc -- :h 'enc'
+    return enc ~= 'utf-8' and enc:upper()
+  end
+}
+
+-- Git block (from heirline cookbook)
+local Git = {
+  condition = conditions.is_git_repo,
+
+  init = function(self)
+    self.status_dict = vim.b.gitsigns_status_dict
+    self.has_changes = self.status_dict.added ~= 0 or self.status_dict.removed ~= 0 or self.status_dict.changed ~= 0
+  end,
+
+  hl = { fg = "orange" },
+
+
+  {   -- git branch name
+    provider = function(self)
+      return " " .. self.status_dict.head
+    end,
+    hl = { bold = true }
+  },
+  -- You could handle delimiters, icons and counts similar to Diagnostics
+  {
+    condition = function(self)
+      return self.has_changes
+    end,
+    provider = "("
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.added or 0
+      return count > 0 and ("+" .. count)
+    end,
+    hl = { fg = "git_add" },
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.removed or 0
+      return count > 0 and ("-" .. count)
+    end,
+    hl = { fg = "git_del" },
+  },
+  {
+    provider = function(self)
+      local count = self.status_dict.changed or 0
+      return count > 0 and ("~" .. count)
+    end,
+    hl = { fg = "git_change" },
+  },
+  {
+    condition = function(self)
+      return self.has_changes
+    end,
+    provider = ")",
+  },
+}
+
+
 -- We're getting minimalists here!
 local Ruler = {
   -- %l = current line number
@@ -316,7 +378,67 @@ local LSPActive = {
   false
   )
 
+  ----------------------
+  -- Configure WinBar --
+  local Navic = {
+    condition = function() return require("nvim-navic").is_available() end,
+    provider = function()
+      return require("nvim-navic").get_location({highlight=true})
+    end,
+    update = 'CursorMoved'
+  }
 
+  local Diagnostics = {
+	  condition = conditions.has_diagnostics,
+
+	  static = {
+		  error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+		  warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+		  info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+		  hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+	  },
+
+	  init = function(self)
+		  self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+		  self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+		  self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+		  self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+	  end,
+
+	  update = { "DiagnosticChanged", "BufEnter" },
+
+	  {
+		  provider = "![",
+	  },
+	  {
+		  provider = function(self)
+			  -- 0 is just another output, we can decide to print it or not!
+			  return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+		  end,
+		  hl = { fg = "diag_error" },
+	  },
+	  {
+		  provider = function(self)
+			  return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+		  end,
+		  hl = { fg = "diag_warn" },
+	  },
+	  {
+		  provider = function(self)
+			  return self.info > 0 and (self.info_icon .. self.info .. " ")
+		  end,
+		  hl = { fg = "diag_info" },
+	  },
+	  {
+		  provider = function(self)
+			  return self.hints > 0 and (self.hint_icon .. self.hints)
+		  end,
+		  hl = { fg = "diag_hint" },
+	  },
+	  {
+		  provider = "]",
+	  },
+  }
 
   ----------------------------
   -- Configure StatusColumn --
@@ -341,15 +463,18 @@ local LSPActive = {
 
   require("heirline").setup({
     statusline = {
+      Git, Space,
       FileNameBlock, Space,
       AlignRight,
-      LSPActive,
-      -- LSPMesages,
+      -- LSPMesages, Space,
+      LSPActive, Space,
+      FileEncoding, Space,
       Ruler,
       ScrollBar,
     },
     winbar = {
-
+      Navic,
+      AlignRight, Diagnostics
     },
     tabline = {
       BufferLine,
