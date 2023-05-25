2e079dfe1fa1deb1731d7741cc8d9713ba824058
Josh Proehl <josh@daedalusdreams.com>
2023-05-25 11:29:58 -0500

    Stub Rust suport into nvim

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -87,6 +87,18 @@ require('gitsigns').setup{
   end
 }
 
+local rt = require("rust-tools")
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
 -- heirline configuration --
 require('heirline_config')
 -- END heirline configuration --
